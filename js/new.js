32edda44054dafd109320bbf20817b8b93830b78
Franklin-Kimatu <frankimatu@gmail.com>
2019-07-11 09:53:26 +0300

    My calculator

---
+++ js/new.js
@@ -0,0 +1,30 @@
+var add =function(number1,number2){
+    return number1 +number2;
+    };
+    var number1 = parseInt(prompt("Enter a number:"));
+    var number2 =parseInt(prompt("Enter another number:"));
+    alert(add(number1,number2));
+var subtract =function(number1,number2){
+    return number1-number2;
+    };
+    var number1 =parseInt(prompt("Enter your first number"));
+    var number2 =parseInt(prompt("Enter yous second value"));
+    alert(subtract(number1,number2));
+var multiply = function(number1,number3){
+    return number1 *number3;
+    };
+    var number1 =parseInt(prompt("Enter your first multiplication Number:"));
+    var number3 =parseInt(prompt("Enter your second Multiplication number"));
+    alert(multiply(number1,number3));
+var division =function(number1,number4){
+    return number1/number4;   
+    };
+    var number1 =parseInt(prompt("Enter your first division number:"));
+    var number4 =parseInt(prompt("Enter you second division number:"));
+    alert(division(number1,number4));
+var modulus =function(number1,number2){
+    return number1 %number2;
+    };
+    var number1=parseInt(prompt("Enter your first number"));
+    var number2=parseInt(prompt("Enter ypour second number:"));
+    alert(modulus(number1,number2));
