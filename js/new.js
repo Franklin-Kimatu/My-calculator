9e0aebe9701a22cbeaa1d553a69e3103512eb55d
Franklin-Kimatu <frankimatu@gmail.com>
2019-07-11 10:06:19 +0300

    Adding result

--- js/new.js
+++ js/new.js
@@ -3,25 +3,29 @@ var add =function(number1,number2){
     };
     var number1 = parseInt(prompt("Enter a number:"));
     var number2 =parseInt(prompt("Enter another number:"));
-    alert(add(number1,number2));
+    var result =(add(number1,number2));
+    alert(result);
 var subtract =function(number1,number2){
     return number1-number2;
     };
     var number1 =parseInt(prompt("Enter your first number"));
     var number2 =parseInt(prompt("Enter yous second value"));
-    alert(subtract(number1,number2));
+    var result=(subtract(number1,number2));
+    alert(result);
 var multiply = function(number1,number3){
     return number1 *number3;
     };
     var number1 =parseInt(prompt("Enter your first multiplication Number:"));
     var number3 =parseInt(prompt("Enter your second Multiplication number"));
-    alert(multiply(number1,number3));
+    var result=(multiply(number1,number3));
+    alert(result);
 var division =function(number1,number4){
     return number1/number4;   
     };
     var number1 =parseInt(prompt("Enter your first division number:"));
     var number4 =parseInt(prompt("Enter you second division number:"));
-    alert(division(number1,number4));
+    var result=(division(number1,number4));
+    alert(result);
 var modulus =function(number1,number2){
     return number1 %number2;
     };
